e33bc259d6d9d898de8579d5a55084a114c0c469
Luke Welbourn <luke.tarquin5@outlook.com>
2023-08-09 14:33:48 +0100

    added reverse functionality, morse -> english, added styles for input and button on main screen.

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import "./style.css";
 
 import { translate } from "./_translate";
 
+import { reverse } from "./_reverse";
+
 const textInput = document.querySelector<HTMLInputElement>("#input-text");
+const morseInput = document.querySelector<HTMLInputElement>("#input-text2");
 const submitButton =
   document.querySelector<HTMLButtonElement>("#submit-button");
 const answerBox = document.querySelector<HTMLElement>(".answer");
+const submitMorse =
+  document.querySelector<HTMLButtonElement>("#submit-button2");
 
-if (!textInput || !submitButton || !answerBox) {
+if (!textInput || !submitButton || !answerBox || !morseInput || !submitMorse) {
   throw new Error("query selector issue");
 }
 
@@ -16,4 +21,9 @@ if (!textInput || !submitButton || !answerBox) {
 submitButton.addEventListener("click", () => {
   const textInputString = textInput.value;
   answerBox.innerHTML = translate(textInputString);
+});
+
+submitMorse.addEventListener("click", () => {
+  const textInputString = morseInput.value;
+  answerBox.innerHTML = reverse(textInputString);
 });
