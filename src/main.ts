5a4174408c53acbf6056b56b189d2f0d7a95dba5
Luke Welbourn <luke.tarquin5@outlook.com>
2023-08-09 10:54:25 +0100

    added function translate, console logging correctly.

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,59 @@ if (!textInput || !submitButton) {
   throw new Error("query selector issue");
 }
 
-const showTextInput = () => {
-  let textInputCurrent = textInput.value;
-  console.log(textInputCurrent);
+const translate = () => {
+  const textInputString = textInput.value;
+  const textInputArray = textInputString.split("");
+
+  const morseCodeMap: { [key: string]: string } = {
+    a: ".-",
+    b: "-...",
+    c: "-.-.",
+    d: "-..",
+    e: ".",
+    f: "..-.",
+    g: "--.",
+    h: "....",
+    i: "..",
+    j: ".---",
+    k: "-.-",
+    l: ".-..",
+    m: "--",
+    n: "-.",
+    o: "---",
+    p: ".--.",
+    q: "--.-",
+    r: ".-.",
+    s: "...",
+    t: "-",
+    u: "..-",
+    v: "...-",
+    w: ".--",
+    x: "-..-",
+    y: "-.--",
+    z: "--..",
+    " ": " ",
+    0: "-----",
+    1: ".----",
+    2: "..---",
+    3: "...--",
+    4: "....-",
+    5: ".....",
+    6: "-....",
+    7: "--...",
+    8: "---..",
+    9: "----.",
+  };
+
+  const morseCodeArray: any = textInputArray.map((character: string) => {
+    if (morseCodeMap.hasOwnProperty(character)) {
+      return morseCodeMap[character];
+    } else {
+      return "?";
+    }
+  });
+  console.log(morseCodeArray);
+  return morseCodeArray;
 };
 
-submitButton.addEventListener("click", showTextInput);
+submitButton.addEventListener("click", translate);
