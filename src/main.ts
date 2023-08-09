3c6f201e3cfd057d238b0115b437d9c1f6092498
Luke Welbourn <luke.tarquin5@outlook.com>
2023-08-09 10:26:57 +0100

    basic html format and query selectors added.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
 import "./style.css";
+
+const textInput = document.querySelector<HTMLInputElement>("#input-text");
+const submitButton =
+  document.querySelector<HTMLButtonElement>("#submit-button");
+
+if (!textInput || !submitButton) {
+  throw new Error("query selector issue");
+}
+
+const showTextInput = () => {
+  let textInputCurrent = textInput.value;
+  console.log(textInputCurrent);
+};
+
+submitButton.addEventListener("click", showTextInput);
